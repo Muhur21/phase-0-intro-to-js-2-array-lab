af9f30794c6f54511a1034163bd600c1a1f300ce
Mehary Gebremariam <mehary.aynealem3@gmail.com>
2023-03-12 08:32:30 -0500

    arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){ 
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    return cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    return cats.shift(name);
+}
+function removeFirstCat(name){
+    return cats.slice(name);
+}
+
+function appendCat(name){
+    const Cats = cats.slice();
+    Cats.push(name);
+    return Cats;
+}
+
+function prependCat(name){
+    const Cats = cats.slice();
+    Cats.unshift(name);
+    return Cats;
+}
+
+function removeLastCat(name){
+    const Cats = cats.slice();
+    Cats.pop(name);
+    return Cats;
+}
+
+function removeFirstCat(name){
+    const Cats = cats.slice();
+    Cats.shift(name);
+    return Cats;
+}
